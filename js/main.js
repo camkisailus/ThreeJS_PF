ab129b42fd83a2c9ba7b7136039018e472b4a090
Cameron Kisailus <camkisailus@gmail.com>
2022-05-11 16:38:50 -0400

    3d models load. Texture doesn't look right

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
-import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+import { TrackballControls } from  '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import {ParticleFilter} from './particle.js'
-
-let scene, camera, renderer, controls, pf;
-
+let scene, camera, renderer, controls, pf, loader;
 
 function init() {
     scene = new THREE.Scene();
@@ -11,10 +10,47 @@ function init() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
     renderer = new THREE.WebGLRenderer({antialias: true});
+    controls = new TrackballControls( camera, renderer.domElement );
+    controls.target.set( 0, 0, 0 );
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    const n = 10;
+
+}
+function init_model(){
+    loader = new GLTFLoader();
+
+    loader.load('../models/apartment/scene.gltf', function(gltf){
+        scene.add(gltf.scene);
+    }, undefined, function (error){
+        console.error(error);
+    });
+    const spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.intensity = 10;
+    spotLight.position.set( 1000, 1000, 1000 );
+    scene.add( spotLight );
+
+    const planeSize = 40;
+
+    const texture_loader = new THREE.TextureLoader();
+    const texture = texture_loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/checker.png');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add(mesh);
+}
+function init_particle_filters(){
+    const n = 500;
     pf = new ParticleFilter(n, 0.01);
     for(let i = 0; i < n; i++){
         pf.particles[i].show(scene);
@@ -22,17 +58,11 @@ function init() {
     for(let i = 0; i < pf.observations.length; i++){
         pf.observations[i].show(scene);
     }
-    // for(let i=0; i < n; i++){
-    //     scene.add(pf.particles[i].mesh)
-    //     scene.add(pf.observations[0].mesh)
-    // }
+    for(let i=0; i < n; i++){
+        scene.add(pf.particles[i].mesh)
+        scene.add(pf.observations[0].mesh)
+    }
 
-    // cube = new Particle(0, 0, 0);
-    // cube2 = new Particle(1, 0, 0);
-    // scene.add(cube2.mesh);
-    // scene.add(cube.mesh);
-    controls = new TrackballControls( camera, renderer.domElement );
-    controls.target.set( 0, 0, 0 );
 }
 
 function animate(){
@@ -48,11 +78,11 @@ function onWindowResize(){
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-document.getElementById("step_filter_update").addEventListener('click', function(){
-    console.log("Clicked!")
-    pf.update_filter();
-});
+// document.getElementById("step_filter_update").addEventListener('click', function(){
+//     pf.update_filter();
+// });
 
 window.addEventListener('resize', onWindowResize, false);
 init();
+init_model();
 animate();
