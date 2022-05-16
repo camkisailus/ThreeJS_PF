87762efdb8b86642124264013cf75595bda3d495
Cameron Kisailus <camkisailus@gmail.com>
2022-05-16 15:34:37 -0400

    Model loads in center. Have kitchen dimensions

--- js/main.js
+++ js/main.js
@@ -5,33 +5,51 @@ import {ParticleFilter} from './particle.js'
 let scene, camera, renderer, controls, pf, loader;
 
 function init() {
+    // Init Scene and Controls
     scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.x = -23.07175289479222;
+    camera.position.y = 41.169479114395514;
+    camera.position.z = -24.941857823187274;
+    camera.rotation._x = -2.144220501675071;
+    camera.rotation._y = -0.4663662532425442;
+    camera.rotation._z = -2.487757754901067;
     renderer = new THREE.WebGLRenderer({antialias: true});
-    controls = new TrackballControls( camera, renderer.domElement );
-    controls.target.set( 0, 0, 0 );
-
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    controls = new TrackballControls( camera, renderer.domElement );
+    controls.target.set( 1,1,1);
 
-}
-function init_model(){
+    // Load GLTF model
     loader = new GLTFLoader();
-
-    loader.load('../models/apartment/scene.gltf', function(gltf){
+    loader.load('../models/centered_apt.glb', function(gltf){
         scene.add(gltf.scene);
     }, undefined, function (error){
         console.error(error);
     });
-    const spotLight = new THREE.SpotLight( 0xffffff );
-    spotLight.intensity = 10;
-    spotLight.position.set( 1000, 1000, 1000 );
-    scene.add( spotLight );
+    const geometry = new THREE.BoxGeometry(10,10,17);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.z = -12;
+    cube.position.x = 5;
+    const box = new THREE.Box3( ).setFromObject( cube );		 
+	const boxHelper = new THREE.Box3Helper( box, 0xffff00 );
+    scene.add( boxHelper );
+    console.log(box.min);
+    console.log(box.max);
+    // scene.add( cube );
+    // Kitchen:
+    //  x: (0, 10)
+    //  y: (-5, 5)
+    //  z: (-20.5, -3.5)
 
-    const planeSize = 40;
 
+    // Init Ambient Light
+    const ambientLight = new THREE.AmbientLight();
+    scene.add(ambientLight);
+
+    // // Init Ground Plane
+    const planeSize = 100;
     const texture_loader = new THREE.TextureLoader();
     const texture = texture_loader.load('https://r105.threejsfundamentals.org/threejs/resources/images/checker.png');
     texture.wrapS = THREE.RepeatWrapping;
@@ -39,7 +57,6 @@ function init_model(){
     texture.magFilter = THREE.NearestFilter;
     const repeats = planeSize / 2;
     texture.repeat.set(repeats, repeats);
-
     const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
     const planeMat = new THREE.MeshPhongMaterial({
       map: texture,
@@ -47,28 +64,28 @@ function init_model(){
     });
     const mesh = new THREE.Mesh(planeGeo, planeMat);
     mesh.rotation.x = Math.PI * -.5;
+    mesh.position.y -= 1;
     scene.add(mesh);
 }
-function init_particle_filters(){
-    const n = 500;
-    pf = new ParticleFilter(n, 0.01);
-    for(let i = 0; i < n; i++){
-        pf.particles[i].show(scene);
-    }
-    for(let i = 0; i < pf.observations.length; i++){
-        pf.observations[i].show(scene);
-    }
-    for(let i=0; i < n; i++){
-        scene.add(pf.particles[i].mesh)
-        scene.add(pf.observations[0].mesh)
-    }
+// function init_particle_filters(){
+//     const n = 500;
+//     pf = new ParticleFilter(n, 0.01);
+//     for(let i = 0; i < n; i++){
+//         pf.particles[i].show(scene);
+//     }
+//     for(let i = 0; i < pf.observations.length; i++){
+//         pf.observations[i].show(scene);
+//     }
+//     for(let i=0; i < n; i++){
+//         scene.add(pf.particles[i].mesh)
+//         scene.add(pf.observations[0].mesh)
+//     }
 
-}
+// }
 
 function animate(){
     requestAnimationFrame(animate);
     controls.update();
-    // pf.update_filter();
     renderer.render(scene, camera);
 }
 
@@ -78,11 +95,37 @@ function onWindowResize(){
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-// document.getElementById("step_filter_update").addEventListener('click', function(){
-//     pf.update_filter();
+document.getElementById("cam_pose").addEventListener('click', function(){
+    console.log(camera.position);
+    console.log(camera.rotation);
+    // console.log(model.position.x);
+});
+// document.getElementById("dec_x_rot").addEventListener('click', function(){
+//     model.rotation.x -= 0.1;
+//     console.log(model.rotation);
+//     // console.log(model.position.x);
+// });
+// document.getElementById("inc_y_pos").addEventListener('click', function(){
+//     model.position.x += 0.5;
+//     console.log(model.position);
+//     // console.log(model.position.x);
+// });
+// document.getElementById("dec_y_pos").addEventListener('click', function(){
+//     model.position.x -= 0.5;
+//     console.log(model.position);
+//     // console.log(model.position.x);
+// });
+// document.getElementById("inc_z_pos").addEventListener('click', function(){
+//     model.position.y += 0.5;
+//     console.log(model.position);
+//     // console.log(model.position.x);
+// });
+// document.getElementById("dec_z_pos").addEventListener('click', function(){
+//     model.position.y -= 0.5;
+//     console.log(model.position);
+//     // console.log(model.position.x);
 // });
 
 window.addEventListener('resize', onWindowResize, false);
 init();
-init_model();
 animate();
